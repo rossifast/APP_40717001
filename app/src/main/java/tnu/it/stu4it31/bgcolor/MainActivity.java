21be0902220fd20dff32a887d2e4a61633fcdbfc
rossifast <56706586+rossifast@users.noreply.github.com>
2021-02-25 15:42:41 +0800

    update

--- app/src/main/java/tnu/it/stu4it31/bgcolor/MainActivity.java
+++ app/src/main/java/tnu/it/stu4it31/bgcolor/MainActivity.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnRed;
+    Button btnRed,btnGreen,btnBlue,btnYellow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         btnRed = findViewById(R.id.btnRed);
         btnRed.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +22,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 View bg = findViewById(android.R.id.content);
                 bg.setBackgroundColor(Color.RED);
+            }
+        });
+        btnGreen = findViewById(R.id.btnGreen);
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View bg = findViewById(android.R.id.content);
+                bg.setBackgroundColor(Color.GREEN);
+            }
+        });
+        btnBlue = findViewById(R.id.btnBlue);
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View bg = findViewById(android.R.id.content);
+                bg.setBackgroundColor(Color.BLUE);
+            }
+        });
+        btnYellow = findViewById(R.id.btnYellow);
+        btnYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View bg = findViewById(android.R.id.content);
+                bg.setBackgroundColor(Color.YELLOW);
             }
         });
     }
